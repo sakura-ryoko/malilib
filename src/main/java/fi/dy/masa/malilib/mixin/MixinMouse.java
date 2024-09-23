e9d9d4a1eb5191e440816ccb6ebe69e7e382ab1d
Sakura Ryoko <sakura@ryoko.soy>
2024-09-23 17:57:12 -0400

    modify InputEventHandler to remove this.mc

--- src/main/java/fi/dy/masa/malilib/mixin/MixinMouse.java
+++ src/main/java/fi/dy/masa/malilib/mixin/MixinMouse.java
@@ -25,14 +25,11 @@ public abstract class MixinMouse
             at = @At(value = "FIELD", target = "Lnet/minecraft/client/Mouse;hasResolutionChanged:Z", ordinal = 0))
     private void hookOnMouseMove(long handle, double xpos, double ypos, CallbackInfo ci)
     {
-        if (this.client != null)
-        {
-            Window window = this.client.getWindow();
-            int mouseX = (int) (((Mouse) (Object) this).getX() * (double) window.getScaledWidth() / (double) window.getWidth());
-            int mouseY = (int) (((Mouse) (Object) this).getY() * (double) window.getScaledHeight() / (double) window.getHeight());
+        Window window = this.client.getWindow();
+        int mouseX = (int) (((Mouse) (Object) this).getX() * (double) window.getScaledWidth() / (double) window.getWidth());
+        int mouseY = (int) (((Mouse) (Object) this).getY() * (double) window.getScaledHeight() / (double) window.getHeight());
 
-            ((InputEventHandler) InputEventHandler.getInputManager()).onMouseMove(mouseX, mouseY);
-        }
+        ((InputEventHandler) InputEventHandler.getInputManager()).onMouseMove(mouseX, mouseY, this.client);
     }
 
     @Inject(method = "onMouseScroll", cancellable = true,
@@ -40,19 +37,16 @@ public abstract class MixinMouse
                     ordinal = 0, shift = At.Shift.AFTER))
     private void hookOnMouseScroll(long handle, double xOffset, double yOffset, CallbackInfo ci)
     {
-        if (this.client != null)
-        {
-            Window window = this.client.getWindow();
-            int mouseX = (int) (((Mouse) (Object) this).getX() * (double) window.getScaledWidth() / (double) window.getWidth());
-            int mouseY = (int) (((Mouse) (Object) this).getY() * (double) window.getScaledHeight() / (double) window.getHeight());
+        Window window = this.client.getWindow();
+        int mouseX = (int) (((Mouse) (Object) this).getX() * (double) window.getScaledWidth() / (double) window.getWidth());
+        int mouseY = (int) (((Mouse) (Object) this).getY() * (double) window.getScaledHeight() / (double) window.getHeight());
 
-            if (((InputEventHandler) InputEventHandler.getInputManager()).onMouseScroll(mouseX, mouseY, xOffset, yOffset))
-            {
-                this.scroller.update(0.0, 0.0);
-                //this.eventDeltaHorizontalWheel = 0.0; --> e
-                //this.eventDeltaVerticalWheel = 0.0; --> f
-                ci.cancel();
-            }
+        if (((InputEventHandler) InputEventHandler.getInputManager()).onMouseScroll(mouseX, mouseY, xOffset, yOffset, this.client))
+        {
+            this.scroller.update(0.0, 0.0);
+            //this.eventDeltaHorizontalWheel = 0.0; --> e
+            //this.eventDeltaVerticalWheel = 0.0; --> f
+            ci.cancel();
         }
     }
 
@@ -60,16 +54,13 @@ public abstract class MixinMouse
             at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;IS_SYSTEM_MAC:Z", ordinal = 0))
     private void hookOnMouseClick(long handle, final int button, final int action, int mods, CallbackInfo ci)
     {
-        if (this.client != null)
-        {
-            Window window = this.client.getWindow();
-            int mouseX = (int) (((Mouse) (Object) this).getX() * (double) window.getScaledWidth() / (double) window.getWidth());
-            int mouseY = (int) (((Mouse) (Object) this).getY() * (double) window.getScaledHeight() / (double) window.getHeight());
+        Window window = this.client.getWindow();
+        int mouseX = (int) (((Mouse) (Object) this).getX() * (double) window.getScaledWidth() / (double) window.getWidth());
+        int mouseY = (int) (((Mouse) (Object) this).getY() * (double) window.getScaledHeight() / (double) window.getHeight());
 
-            if (((InputEventHandler) InputEventHandler.getInputManager()).onMouseClick(mouseX, mouseY, button, action))
-            {
-                ci.cancel();
-            }
+        if (((InputEventHandler) InputEventHandler.getInputManager()).onMouseClick(mouseX, mouseY, button, action, this.client))
+        {
+            ci.cancel();
         }
     }
 }
