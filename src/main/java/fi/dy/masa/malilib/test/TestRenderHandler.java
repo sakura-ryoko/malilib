9d6530cd9f71c0f4eb4a078038baba827d7ce35b
Sakura Ryoko <sakura@ryoko.soy>
2024-12-29 19:18:41 -0500

    fix: Convert all Pair objects to Apache Commons. fix: Tooltip rendering for InventoryOverlayScreen handling for calling the `IRender.onRenderTooltipLast` handling feat: Item Tooltip handlers for inserting text into Item Tooltips at 3 places; First, Middle, and Last. feat: Migrate InventoryOverlayScreen to MaLiLib and add a common Interface for downstream mods called `IInventoryOverlayHandler` with an accompanying `IDataSyncer` interface as just a common way to call the mods' assigned Data Syncer.

--- src/main/java/fi/dy/masa/malilib/test/TestRenderHandler.java
+++ src/main/java/fi/dy/masa/malilib/test/TestRenderHandler.java
@@ -1,14 +1,21 @@
 package fi.dy.masa.malilib.test;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
+import javax.annotation.Nullable;
+import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.ApiStatus;
 import org.joml.Matrix4f;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.CrafterBlockEntity;
+import net.minecraft.block.enums.ChestType;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.Camera;
@@ -24,9 +31,13 @@ import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.World;
 
 import fi.dy.masa.malilib.MaLiLib;
 import fi.dy.masa.malilib.MaLiLibConfigs;
@@ -38,24 +49,66 @@ import fi.dy.masa.malilib.render.RenderUtils;
 import fi.dy.masa.malilib.util.*;
 import fi.dy.masa.malilib.util.nbt.NbtBlockUtils;
 
+@ApiStatus.Experimental
 public class TestRenderHandler implements IRenderer
 {
+    private static final TestRenderHandler INSTANCE = new TestRenderHandler();
+
+    public TestRenderHandler()
+    {
+        // NO-OP
+    }
+
+    public static TestRenderHandler getInstance()
+    {
+        return INSTANCE;
+    }
+
     @Override
     public void onRenderGameOverlayLastDrawer(DrawContext drawContext, float partialTicks, Profiler profiler, MinecraftClient mc)
     {
-        if (MaLiLibConfigs.Test.TEST_CONFIG_BOOLEAN.getBooleanValue() && GuiBase.isAltDown())
+        if (MaLiLibConfigs.Test.TEST_CONFIG_BOOLEAN.getBooleanValue() &&
+            MaLiLibConfigs.Test.TEST_INVENTORY_OVERLAY.getKeybind().isKeybindHeld())
         {
+            /*
             profiler.push(this.getProfilerSectionSupplier() + "_inventory_overlay");
-            //renderInventoryOverlay(mc, drawContext);
-            InventoryOverlay.Context context = RayTraceUtils.getTargetInventory(mc);
+            InventoryOverlay.Context context = RayTraceUtils.getTargetInventory(mc, true);
 
             if (context != null)
             {
                 renderInventoryOverlay(context, drawContext, mc);
             }
+
             profiler.pop();
+             */
+
+            TestInventoryOverlayHandler.getInstance().getRenderContext(drawContext, profiler, mc);
         }
     }
+
+    @Override
+    public void onRenderGameOverlayPost(DrawContext drawContext)
+    {
+        // NO-OP
+    }
+
+    /*
+    public void renderInventoryOverlay(InventoryOverlay.Context context, DrawContext drawContext, MinecraftClient mc)
+    {
+        if (MaLiLibConfigs.Test.TEST_INVENTORY_OVERLAY_OG.getBooleanValue())
+        {
+            // Tweakeroo style
+            renderInventoryOverlayOG(context, drawContext, mc);
+        }
+        else
+        {
+            // MiniHUD style
+            var screen = new InventoryOverlayScreen(context);
+            screen.init(mc, 0, 0);
+            screen.render(drawContext, 0, 0, 0);
+        }
+    }
+     */
 
     @Override
     public void onRenderWorldLast(Matrix4f posMatrix, Matrix4f projMatrix)
@@ -77,6 +130,39 @@ public class TestRenderHandler implements IRenderer
         }
     }
 
+    @Override
+    public void onRenderTooltipComponentInsertFirst(Item.TooltipContext context, ItemStack stack, List<Text> list)
+    {
+        if (MaLiLibConfigs.Test.TEST_CONFIG_BOOLEAN.getBooleanValue())
+        {
+            // This can cause various problems unrelated to the tooltips; but it does work.
+            /*
+            MutableText itemName = list.getFirst().copy();
+            MutableText title = Text.empty().append(StringUtils.translateAsText(MaLiLibReference.MOD_ID+".gui.tooltip.test.title"));
+            list.addFirst(title);
+             */
+            list.add(StringUtils.translateAsText(MaLiLibReference.MOD_ID+".gui.tooltip.test.first"));
+        }
+    }
+
+    @Override
+    public void onRenderTooltipComponentInsertMiddle(Item.TooltipContext context, ItemStack stack, List<Text> list)
+    {
+        if (MaLiLibConfigs.Test.TEST_CONFIG_BOOLEAN.getBooleanValue())
+        {
+            list.add(StringUtils.translateAsText(MaLiLibReference.MOD_ID+".gui.tooltip.test.middle"));
+        }
+    }
+
+    @Override
+    public void onRenderTooltipComponentInsertLast(Item.TooltipContext context, ItemStack stack, List<Text> list)
+    {
+        if (MaLiLibConfigs.Test.TEST_CONFIG_BOOLEAN.getBooleanValue())
+        {
+            list.add(StringUtils.translateAsText(MaLiLibReference.MOD_ID+".gui.tooltip.test.last"));
+        }
+    }
+
     //@Override
     // TODO 1.21.3+
     //public void onRenderWorldPreWeather(Matrix4f posMatrix, Matrix4f projMatrix, Frustum frustum, Camera camera, Fog fog, Profiler profiler)
@@ -88,7 +174,7 @@ public class TestRenderHandler implements IRenderer
 
             profiler.push(MaLiLibReference.MOD_ID + "_test_walls");
 
-            if (TestEnumConfig.TEST_WALLS_HOTKEY.getBooleanValue())
+            if (ConfigTestEnum.TEST_WALLS_HOTKEY.getBooleanValue())
             {
                 if (TestWalls.needsUpdate(camera.getBlockPos()))
                 {
@@ -357,13 +443,20 @@ public class TestRenderHandler implements IRenderer
             InventoryOverlay.renderEquipmentStacks(entityLivingBase, x, y, mc, drawContext);
         }
         */
-    public static void renderInventoryOverlay(InventoryOverlay.Context context, DrawContext drawContext, MinecraftClient mc)
+
+    // OG / Tweakeroo method also
+    public static void renderInventoryOverlayOG(InventoryOverlay.Context context, DrawContext drawContext, MinecraftClient mc)
     {
         //MinecraftClient mc = MinecraftClient.getInstance();
         LivingEntity entityLivingBase = null;
         BlockEntity be = null;
         Inventory inv = null;
         NbtCompound nbt = new NbtCompound();
+
+        if (context == null)
+        {
+            return;
+        }
 
         if (context.be() != null)
         {
@@ -507,5 +600,30 @@ public class TestRenderHandler implements IRenderer
             InventoryOverlay.renderEquipmentOverlayBackground(x, y, entityLivingBase, drawContext);
             InventoryOverlay.renderEquipmentStacks(entityLivingBase, x, y, mc, drawContext);
         }
+    }
+
+    @Nullable
+    public Pair<BlockEntity, NbtCompound> requestBlockEntityAt(World world, BlockPos pos)
+    {
+        if (!(world instanceof ServerWorld))
+        {
+            Pair<BlockEntity, NbtCompound> pair = TestDataSyncer.getInstance().requestBlockEntity(world, pos);
+
+            BlockState state = world.getBlockState(pos);
+
+            if (state.getBlock() instanceof ChestBlock)
+            {
+                ChestType type = state.get(ChestBlock.CHEST_TYPE);
+
+                if (type != ChestType.SINGLE)
+                {
+                    return TestDataSyncer.getInstance().requestBlockEntity(world, pos.offset(ChestBlock.getFacing(state)));
+                }
+            }
+
+            return pair;
+        }
+
+        return null;
     }
 }
