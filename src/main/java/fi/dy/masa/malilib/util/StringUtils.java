91d1f602ba3fb231e39c6c0ed1e68533b9319889
Sakura Ryoko <sakura@ryoko.soy>
2025-02-08 14:19:14 -0500

    Update DataDump and StringUtils with Path files.

--- src/main/java/fi/dy/masa/malilib/util/StringUtils.java
+++ src/main/java/fi/dy/masa/malilib/util/StringUtils.java
@@ -2,6 +2,7 @@ package fi.dy.masa.malilib.util;
 
 import java.io.File;
 import java.net.SocketAddress;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -173,8 +174,16 @@ public class StringUtils
     {
         Text name = Text.literal(file.getName())
             .formatted(net.minecraft.util.Formatting.UNDERLINE)
-            //.styled((style) -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, file.getAbsolutePath())));
             .styled((style) -> style.withClickEvent(new ClickEvent.OpenFile(file.getAbsolutePath())));
+
+        sender.sendMessage(Text.translatable(messageKey, name), false);
+    }
+
+    public static void sendOpenFileChatMessage(PlayerEntity sender, String messageKey, Path file)
+    {
+        Text name = Text.literal(file.getFileName().toString())
+                        .formatted(net.minecraft.util.Formatting.UNDERLINE)
+                        .styled((style) -> style.withClickEvent(new ClickEvent.OpenFile(file.toAbsolutePath())));
 
         sender.sendMessage(Text.translatable(messageKey, name), false);
     }
