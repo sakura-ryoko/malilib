d432c4a2faa0a8f44e1b0d4913b3246223591a15
Sakura Ryoko <sakura@ryoko.soy>
2025-03-03 01:21:19 -0500

    fix: https://github.com/maruohon/minihud/issues/378#issuecomment-2693315304

--- src/main/java/fi/dy/masa/malilib/render/RenderUtils.java
+++ src/main/java/fi/dy/masa/malilib/render/RenderUtils.java
@@ -1678,8 +1678,13 @@ public class RenderUtils
 
     public static void setVillagerBackgroundTintColor(VillagerData data, boolean useBgColors)
     {
-        VillagerProfession profession = data != null ? data.getProfession() : null;
-        setVillagerBackgroundTintColor(profession, useBgColors);
+        if (useBgColors)
+        {
+            VillagerProfession profession = data != null ? data.getProfession() : null;
+            setVillagerBackgroundTintColor(profession, useBgColors);
+        }
+
+        return color(1f, 1f, 1f, 1f);
     }
 
     public static void setVillagerBackgroundTintColor(VillagerProfession profession, boolean useBgColors)
@@ -1701,6 +1706,8 @@ public class RenderUtils
 
     public static DyeColor getVillagerColor(VillagerProfession profession)
     {
+        if (profession == null) return null;
+
         if (profession.equals(VillagerProfession.NONE))
         {
             return DyeColor.BLUE;
