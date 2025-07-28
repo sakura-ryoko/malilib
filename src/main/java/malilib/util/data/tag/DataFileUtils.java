0bac2d2adb6df1f7ff97200a4bdeb24267d2fbe1
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2025-07-28 16:58:01 +0300

    Add a DataFileUtils class stub

---
+++ src/main/java/malilib/util/data/tag/DataFileUtils.java
@@ -0,0 +1,19 @@
+package malilib.util.data.tag;
+
+import java.nio.file.Path;
+
+import javax.annotation.Nullable;
+
+public class DataFileUtils
+{
+    @Nullable
+    public static CompoundData readCompoundDataNbtFromFile(Path fileIn)
+    {
+        return null;
+    }
+
+    public static boolean writeCompressedCompoundData(Path fileOut, CompoundData dataIn)
+    {
+        return true;
+    }
+}
