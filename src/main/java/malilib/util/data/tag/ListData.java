30b2c34d658038d7f815cb105019b0b5664ea013
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2025-07-30 17:30:01 +0300

    ListData: Add most of the missing methods

--- src/main/java/malilib/util/data/tag/ListData.java
+++ src/main/java/malilib/util/data/tag/ListData.java
@@ -26,6 +26,108 @@ public class ListData extends BaseData
         return this.list.size();
     }
 
+    public void clear()
+    {
+        this.list.clear();
+    }
+
+    public boolean remove(int index)
+    {
+        if (index < this.list.size())
+        {
+            this.list.remove(index);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean add(BaseData entry)
+    {
+        if (entry.getType() != this.containedType)
+        {
+            return false;
+        }
+
+        this.list.add(entry);
+        return true;
+    }
+
+    public BaseData get(int index)
+    {
+        return this.list.get(index);
+    }
+
+    public byte getByteAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_BYTE)
+        {
+            return ((ByteData) this.list.get(index)).value;
+        }
+
+        return 0;
+    }
+
+    public short getShortAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_SHORT)
+        {
+            return ((ShortData) this.list.get(index)).value;
+        }
+
+        return 0;
+    }
+
+    public int getIntAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_INT)
+        {
+            return ((IntData) this.list.get(index)).value;
+        }
+
+        return 0;
+    }
+
+    public long getLongAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_LONG)
+        {
+            return ((LongData) this.list.get(index)).value;
+        }
+
+        return 0;
+    }
+
+    public float getFloatAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_FLOAT)
+        {
+            return ((FloatData) this.list.get(index)).value;
+        }
+
+        return 0.0f;
+    }
+
+    public double getDoubleAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_DOUBLE)
+        {
+            return ((DoubleData) this.list.get(index)).value;
+        }
+
+        return 0.0;
+    }
+
+    public CompoundData getCompoundAt(int index)
+    {
+        if (this.containedType == Constants.NBT.TAG_COMPOUND)
+        {
+            return (CompoundData) this.list.get(index);
+        }
+
+        return new CompoundData();
+    }
+
     @Override
     public BaseData copy()
     {
