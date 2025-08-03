18fd96fd56f092bf17e0829527e804b65014d50f
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2025-08-03 19:33:47 +0300

    MinecraftVersion: Fix requesting old versions based on unknown DataVersion

--- src/main/java/malilib/util/game/MinecraftVersion.java
+++ src/main/java/malilib/util/game/MinecraftVersion.java
@@ -120,7 +120,7 @@ public class MinecraftVersion
     public static final MinecraftVersion MC_1_7_1  = new MinecraftVersion("1.7.1",  -1, 3);
     public static final MinecraftVersion MC_1_7    = new MinecraftVersion("1.7",    -1, 3);
 
-    public static final MinecraftVersion MC_UNKNOWN = new MinecraftVersion("?",  -1, -1);
+    public static final MinecraftVersion MC_UNKNOWN = new MinecraftVersion("< 1.9",  -1, -1);
 
     public static final ImmutableList<MinecraftVersion> MINECRAFT_RELEASE_VERSIONS = ImmutableList.of(
         MC_1_7,
@@ -232,6 +232,11 @@ public class MinecraftVersion
 
     public static MinecraftVersion getVersionOrCreateSnapshotVersionByDataVersion(int dataVersion)
     {
+        if (dataVersion < 0)
+        {
+            return MC_UNKNOWN;
+        }
+
         for (MinecraftVersion v : MINECRAFT_RELEASE_VERSIONS)
         {
             if (v.dataVersion == dataVersion)
