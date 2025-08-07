3b5c9c2397da91f542f20dbf7279a13ad3d7fb47
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2025-08-07 19:17:14 +0300

    BlockState: Various additions

--- src/main/java/malilib/util/world/BlockState.java
+++ src/main/java/malilib/util/world/BlockState.java
@@ -6,26 +6,75 @@ import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
 
+import malilib.MaLiLibReference;
+import malilib.util.data.Identifier;
 import malilib.util.data.tag.CompoundData;
 import malilib.util.game.BlockUtils;
+import malilib.util.game.wrap.RegistryUtils;
 import malilib.util.position.BlockMirror;
 import malilib.util.position.BlockRotation;
 
 public class BlockState
 {
+    public static final BlockState INVALID = new BlockState(Blocks.AIR.getDefaultState(),
+                                                            new Identifier(MaLiLibReference.MOD_ID, "invalid_block"),
+                                                            Block.REGISTRY.getIDForObject(Blocks.AIR));
+
     private static final Object2ObjectOpenHashMap<IBlockState, BlockState> MAP = new Object2ObjectOpenHashMap<>();
 
     public static BlockState of(IBlockState vanillaState)
     {
-        return MAP.computeIfAbsent(vanillaState, BlockState::new);
+        return MAP.computeIfAbsent(vanillaState, BlockState::createState);
+    }
+
+    public static BlockState fromRegistryName(ResourceLocation id)
+    {
+        return of(RegistryUtils.getBlockById(id).getDefaultState());
+    }
+
+    public static BlockState fromRegistryNameStr(String idStr)
+    {
+        return of(RegistryUtils.getBlockById(new Identifier(idStr)).getDefaultState());
+    }
+
+    protected static BlockState createState(IBlockState vanillaState)
+    {
+        ResourceLocation rl = RegistryUtils.getBlockId(vanillaState);
+        Identifier registryName;
+        int blockId;
+
+        if (rl != null)
+        {
+            registryName = new Identifier(rl);
+            blockId = Block.REGISTRY.getIDForObject(vanillaState.getBlock());
+        }
+        else
+        {
+            registryName = new Identifier(MaLiLibReference.MOD_ID, "invalid_block");
+            blockId = Block.REGISTRY.getIDForObject(Blocks.AIR);
+        }
+
+        return new BlockState(vanillaState, registryName, blockId);
     }
 
     protected final IBlockState vanillaState;
+    protected final Identifier registryName;
+    protected final int blockId;
 
-    protected BlockState(IBlockState vanillaState)
+    protected BlockState(IBlockState vanillaState, Identifier registryName, int blockId)
     {
         this.vanillaState = vanillaState;
+        this.registryName = registryName;
+        this.blockId = blockId;
+    }
+
+    protected BlockState()
+    {
+        this.vanillaState = Blocks.AIR.getDefaultState();
+        this.registryName = new Identifier(MaLiLibReference.MOD_ID, "invalid_block");
+        this.blockId = Block.REGISTRY.getIDForObject(this.vanillaState.getBlock());
     }
 
     public IBlockState vanillaState()
@@ -36,6 +85,22 @@ public class BlockState
     public Block getBlock()
     {
         return this.vanillaState.getBlock();
+    }
+
+    public Identifier getRegistryName()
+    {
+        return this.registryName;
+    }
+
+    public String getRegistryNameStr()
+    {
+        return this.registryName.toString();
+    }
+
+    // TODO This needs to go away for 1.13+ compatibility
+    public int getOldBlockId()
+    {
+        return this.blockId;
     }
 
     public BlockState withRotation(BlockRotation rotation)
