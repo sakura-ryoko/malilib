503d944e2150ce03b3ab1ff327579ca2e220d7ac
Sakura Ryoko <sakura@ryoko.soy>
2025-11-26 20:45:06 -0500

    Remove check for DEBUG_MODE from Shared Constants.

--- src/main/java/fi/dy/masa/malilib/MaLiLibReference.java
+++ src/main/java/fi/dy/masa/malilib/MaLiLibReference.java
@@ -15,22 +15,7 @@ public class MaLiLibReference
     public static final int MC_DATA_VERSION = SharedConstants.getGameVersion().dataVersion().id();
 	public static final Path GAME_DIR = FabricLoader.getInstance().getGameDir();
 	public static final Path CONFIG_DIR = FabricLoader.getInstance().getConfigDir();
-    public static final boolean DEBUG_MODE = isDebug();
-    public static final boolean LOCAL_DEBUG = false;
+    public static final boolean DEBUG_MODE = false;
     public static final boolean ANSI_MODE = DEBUG_MODE;
     public static final boolean EXPERIMENTAL_MODE = false;
-
-    /**
-     * This is so that MaLiLib's DEBUG_MODE is toggle-able using Vanilla's Debug Flag(s).
-     * @return ()
-     */
-    private static boolean isDebug()
-    {
-        if (SharedConstants.DEBUG_ENABLED || SharedConstants.isDevelopment)
-        {
-            return true;
-        }
-
-        return LOCAL_DEBUG;
-    }
 }
