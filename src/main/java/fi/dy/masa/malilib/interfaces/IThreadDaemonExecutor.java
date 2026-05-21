a8b6de66462f457276beec702fde932816eb6350
Sakura Ryoko <sakura@ryoko.soy>
2026-05-21 18:56:52 -0400

    fix: Thread spin-locks after sleep()

--- src/main/java/fi/dy/masa/malilib/interfaces/IThreadDaemonExecutor.java
+++ src/main/java/fi/dy/masa/malilib/interfaces/IThreadDaemonExecutor.java
@@ -138,9 +138,12 @@ public interface IThreadDaemonExecutor<T extends IThreadTaskBase> extends Runnab
 			}
 			finally
 			{
-				// This is required to avoid spin-lock.
-				MaLiLib.debugLog("IThreadDaemonExecutor#Executor: sleep finished: for '{}'", this.currentThreadName());
-				this.resume();
+				if (this.isPaused())
+				{
+					// This is required to avoid spin-lock.
+					MaLiLib.debugLog("IThreadDaemonExecutor#Executor: sleep ended: for '{}'", this.currentThreadName());
+					this.resume();
+				}
 			}
 		}
 	}
