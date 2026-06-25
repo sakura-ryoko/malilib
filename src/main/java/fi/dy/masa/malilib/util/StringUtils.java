4c76d9385cf7abb61d9962e780b4cd08aef8cf89
Sakura Ryoko <sakura@ryoko.soy>
2026-06-25 18:50:48 -0400

    add: translatable Empty Keybind button text

--- src/main/java/fi/dy/masa/malilib/util/StringUtils.java
+++ src/main/java/fi/dy/masa/malilib/util/StringUtils.java
@@ -937,4 +937,25 @@ public class StringUtils
             return null;
         }
     }
+
+    /**
+     * Return if the given CharSequence is blank
+     * @param cs -
+     * @return -
+     */
+    public static boolean isBlank(final CharSequence cs)
+    {
+        final int strLen = cs == null ? 0 : cs.length();
+        if (strLen == 0) { return true; }
+
+        for (int i = 0; i < strLen; i++)
+        {
+            if (!Character.isWhitespace(cs.charAt(i)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
